e239895fd5f97c2571e700b2172ad6f1ca805d8c
stefanoreza <stefanorezasaliscipta@gmail.com>
2024-10-08 17:48:46 +0700

    message: final commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// ini javascript
+// const convertCelsiusToFahrenheitInConciseSyntax = 
+//   (temperature) => (9 / 5) * temperature + 32;
+function convert() {
+  const celsius = parseFloat(document.getElementById('celsius').value);
+  const fahrenheit = (celsius * 9/5) + 32;
+  document.getElementById('fahrenheit').value = fahrenheit;
+  document.getElementById('kalkulasi').value = `${celsius}°C * (9/5) + 32 = ${fahrenheit}°F`;
+}
+
+function reset() {
+  document.getElementById('celsius').value = '';
+  document.getElementById('fahrenheit').value = '';
+  document.getElementById('kalkulasi').value = '';
+}
+
+function reverse() {
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
+  const celsius = (fahrenheit - 32) * 5/9;
+  document.getElementById('celsius').value = celsius;
+  document.getElementById('kalkulasi').value = `(${fahrenheit}°F - 32°) * (5/9) = ${celsius}°C`;
+}
